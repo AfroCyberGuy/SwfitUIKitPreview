7da74bedcee838397fc2484b5e2970368963aad2
tatendakabike <tkabike@gmail.com>
2019-11-19 09:47:58 +0200

    Initial Commit

---
+++ SwiftPreview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftPreview
+//
+//  Created by Tatenda Kabike on 2019/11/19.
+//  Copyright © 2019 Tatenda Kabike. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
